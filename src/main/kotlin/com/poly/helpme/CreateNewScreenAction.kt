539df72b73aecb74e35547171c794387fabe1cd5
Jérémy Girerd-Rey <jeremy.girerdrey@gmail.com>
2025-08-08 18:52:46 +0200

    Rework

--- src/main/kotlin/com/poly/helpme/CreateNewScreenAction.kt
+++ src/main/kotlin/com/poly/helpme/CreateNewScreenAction.kt
@@ -6,11 +6,11 @@ class CreateNewScreenAction : AbstractAction() {
     override val title = "Enter screen Name (e.g., Home):"
     override val CTA = "Create new screen"
     override var files = listOf<Pair<String, String>>(
-        "ComposableScreen.mustache" to "Screen.kt",
+        "Screen.mustache" to "Screen.kt",
         "ViewModel.mustache" to "ViewModel.kt",
-        "State.mustache" to "State.kt",
-        "Events.mustache" to "Events.kt",
-        "SideEffect.mustache" to "SideEffect.kt"
+        "ScreenState.mustache" to "State.kt",
+        "Event.mustache" to "Events.kt",
+        "EventFromVM.mustache" to "SideEffect.kt"
     )
 
     override fun actionPerformed(event: AnActionEvent) {
